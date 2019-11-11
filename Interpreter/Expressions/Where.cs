be4529a82b2ff7ead0251bb7688a92ae511bd195
Jordan Robinson <jordan.robinson@codurance.com>
2019-11-11 17:24:25 +0000

    Adds Interpreter

---
+++ Interpreter/Expressions/Where.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Interpreter.Database;
+
+namespace Interpreter.Expressions
+{
+	public class Where : IExpression
+	{
+		private readonly Func<string, bool> _filter;
+
+		public Where(Func<string, bool> filter)
+		{
+			_filter = filter;
+		}
+
+		public List<string> Interpret(Context context)
+		{
+			context.WhereFilter = _filter;
+			return context.Search();
+		}
+	}
+}
